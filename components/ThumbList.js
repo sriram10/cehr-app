44bfde0399c83b891ed8faac7998960d0b7b9fdf
santhoshkumar-fox <santhoshkumar.v@pentafox.in>
2022-12-05 19:31:58 +0530

    05decnight

---
+++ components/ThumbList.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const ThumbList = () => {
+  return (
+    <View>
+      <Text>ThumbList</Text>
+    </View>
+  )
+}
+
+export default ThumbList
+
+const styles = StyleSheet.create({})
